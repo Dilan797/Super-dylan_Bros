e971e865b7258b678d813b2c16d0f65fe16bffc1
Dilan797 <dilanblues@gmail.com>
2024-07-22 23:01:49 -0500

    first commit-22/07/2024 Dilan-Bros

--- game.js
+++ game.js
@@ -2,9 +2,9 @@
 //Global Phaser
 const config = {
     type: Phaser.AUTO, // webgl, canvas
-    width: 256,//Tamaño del juego
-    height:244,
-    backgrounColor: '#fff',
+    width: 270,//Tamaño del juego
+    height:255,
+    backgroundColor: '#049cd8',
     parent:'game', 
     scene:{
         preload, //Funcion que se ejecuta para precargar recursos
@@ -17,11 +17,53 @@ new Phaser.Game(config)
 
 
 function preload () {
-    console.log('preload')
+//Cargamos los recursos
+    this.load.image(//Cargamos una nueve
+        'cloud1',
+        'assets/scenery/overworld/cloud1.png'
+    )
+    this.load.image(//Cargamos una nueve
+        'floorbrick',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+    //Imagen con más de una posición
+    this.load.spritesheet(
+        'mario', //<---- id
+        'assets/entities/mario.png',
+        { frameWidth: 18, frameHeight: 16}//Se pasa el espació que ocupa cada  
+    )                                     //uno de los Marios
 }
 
 function create () {
-    console.log('create')
-}
+    //Mostramos la imagen
+    // image (x, y, id-del-assets)
+    //Styles cloud
+    this.add.image(0, 0, 'cloud1')
+        .setOrigin(0, 0)
+        .setScale(0.15)//Reduce la imagen
 
-function update () {}
+    //Ponemos una imagen y una textura
+    this.add.tileSprite(0, config.height -32, config.width, 32,
+        'floorbrick')
+        .setOrigin(0, 0)//Empieza la imagen desde el punto 0,0
+        
+    this.mario=this.add.sprite(50, 222, 'mario')
+        .setOrigin(0, 1);//Styles  Mario
+    
+    this.keys = this.input.keyboard.createCursorKeys()
+    // Añadir manejo de errores para la carga de assets
+    this.load.on('loaderror', (file) => {
+        console.error('Error loading asset:', file.src);
+    });
+}
+//Todos los juegos del mundo funcionan con 
+// un bucle infinito que se ejecuta en cada frame
+
+function update () {
+    if(this.keys.left.isDown){//Movimiento de Mario
+        this.mario.x -=2
+    } else if (this.keys.right.isDown) {
+        this.mario.x +=2
+    }
+
+}
